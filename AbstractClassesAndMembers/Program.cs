7ef1f13ab45879faec8e0fa52b057ccbcacb7d97
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-07 16:08:26 -0600

    clean up codes

--- AbstractClassesAndMembers/Program.cs
+++ AbstractClassesAndMembers/Program.cs
@@ -10,6 +10,11 @@ namespace AbstractClassesAndMembers // Note: actual namespace depends on the pro
 
         static void Main(string[] args)
         {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
             
         }
     }
--- AbstractClassesAndMembers/Shape.cs
+++ AbstractClassesAndMembers/Shape.cs
@@ -6,16 +6,36 @@ using System.Threading.Tasks;
 
 namespace AbstractClassesAndMembers
 {
-    public class Shape
+    public abstract class Shape
     {
 
             public int Width { get; set; }
             public int Height { get; set; }
 
-            public virtual void Draw()
-            {
-                //leave this method empty
-            }
+        public abstract void Draw();
+        
+        public void Copy()
+        {
+            Console.WriteLine("Copy shape into clipboard.");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("Select the shape.");
+        }
         
     }
 }
+
+
+/*
+ So here is the benefit of abstract modifier.
+
+It's not something that you use every day in your programming career but it's a tool that you can use
+
+when you're facing situations like this when you want to force your design to be followed everywhere
+
+in the code base.
+
+Now before we finished this lecture I want to show you a couple more things.
+ */
